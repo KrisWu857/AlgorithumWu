709b5290fd977689598c87adf654ef5a7f68f2bc
wangdada <xx@qq.com>
2024-03-22 22:46:50 +0800

    test

--- src/com/class101/Code02_DeleteAgainAndAgain.java
+++ src/com/class101/Code02_DeleteAgainAndAgain.java
@@ -9,11 +9,77 @@ package com.class101;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 
+import java.io.*;
+
 public class Code02_DeleteAgainAndAgain {
+    public static char[] s1, s2;
 
-    public static char[]s1 ,s2 ;
+    public static int MAXN = 1000001;
 
-    public static int
+    public static int[] next = new int[MAXN];
 
+    public static int[] stack1 = new int[MAXN];
 
+    public static int[] stack2 = new int[MAXN];
+
+    public static int size;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        s1 = in.readLine().toCharArray();
+        s2 = in.readLine().toCharArray();
+        compute();
+        for (int i = 0; i < size; i++) {
+            out.print(s1[stack1[i]]);
+        }
+        out.println();
+        out.flush();
+        out.close();
+        in.close();
+    }
+
+    public static void compute() {
+        size = 0;
+        int n = s1.length, m = s2.length, x = 0, y = 0;
+        nextArray(m);
+        while (x < n) {
+            // x位置上的字符与y位置上的字符对应上
+            if (s1[x] == s2[y]) {
+
+                stack1[size] = x;
+                stack2[size] = y;
+                size++;
+                x++;
+                y++;
+            } else if (y == 0) {
+                stack1[size] = x;
+                stack2[size] = -1;
+                size++;
+                x++;
+            } else {
+                y = next[y];
+            }
+            if (y == m) {
+                // 相当于栈直接弹出了m条记录
+                size -= m;
+                y = size > 0 ? (stack2[size - 1] + 1) : 0;
+            }
+        }
+    }
+
+    public static void nextArray(int m) {
+        next[0] = -1;
+        next[1] = 0;
+        int i = 2, cn = 0;
+        while (i < m) {
+            if (s2[i - 1] == s2[cn]) {
+                next[i++] = ++cn;
+            } else if (cn > 0) {
+                cn = next[cn];
+            } else {
+                next[i++] = 0;
+            }
+        }
+    }
 }
---
+++ src/com/class101/Code03_LinkedListInBinaryTree.java
@@ -0,0 +1,4 @@
+package com.class101;
+
+public class Code03_LinkedListInBinaryTree {
+}
